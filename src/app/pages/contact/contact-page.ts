a5c6329e0ee3be5824243a6d1b898ed197531b59
Andrea <guerreroaanathalia@gmail.com>
2026-03-11 11:31:36 -0500

    ssr

--- src/app/pages/contact/contact-page.ts
+++ src/app/pages/contact/contact-page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
   imports: [],
   templateUrl: './contact-page.html',
 })
-export default class ContactPage {
+export default class ContactPage implements OnInit {
+  private title = inject(Title)
+  private meta = inject(Meta)
 
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page')
+    this.meta.updateTag({name: 'description', content:' este es mi contact page'})
+    this.meta.updateTag({name: 'og:title', content:' Contact Page'})
+    this.meta.updateTag({name: 'keywords', content:'hola,mundo,angular,ssr'})
+  }
 }
--- src/app/pages/pricing/pricing-page.ts
+++ src/app/pages/pricing/pricing-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './pricing-page.html',
 })
 export default class PricingPage {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID); //verficar si estamos del lado del cliente o del servidor
 
+  ngOnInit(): void {
+    // console.log("doc",document)
+    console.log('hello word'); // el codigo se ejecuta del lado del servidor y del lado del cliente
+    this.title.setTitle('Pricing Page')
+    this.meta.updateTag({name: 'description', content:' este es mi pricing page'})
+    this.meta.updateTag({name: 'og:title', content:' Pricing Page'})
+    this.meta.updateTag({name: 'keywords', content:'hola,mundo,angular,ssr'})
+
+    console.log('platform', this.platform);
+    console.log(isPlatformServer(this.platform));
+    //mala practica
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'pricing page'; // document no esat definido en el server
+    // }
+  }
 }
