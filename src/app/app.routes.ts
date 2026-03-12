a53092a435a8c834d76fde04697663de17240f26
Andrea <guerreroaanathalia@gmail.com>
2026-03-12 17:36:35 -0500

    cambio en el funcionamiento de la paginacion de queyparams a params , cambio en la ruta de los pokemons se cambio el id por el nombre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,8 +13,12 @@ export const routes: Routes = [
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact-page'),
   },
+  // {
+  //   path: 'pokemons',
+  //   loadComponent: () => import('./pages/pokemons/pokemons-page'),
+  // },
   {
-    path: 'pokemons',
+    path: 'pokemons/page/:page',
     loadComponent: () => import('./pages/pokemons/pokemons-page'),
   },
   {
--- src/app/pages/pokemons/pokemons-page.ts
+++ src/app/pages/pokemons/pokemons-page.ts
@@ -1,8 +1,8 @@
-import { ApplicationRef, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ApplicationRef, Component, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonList } from '../../pokemons/components/pokemon-list/pokemon-list';
 import { PokemonServices } from '../../pokemons/services/pokemon.services';
 import { SimplePokemon } from '../../pokemons/interfaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, tap, pipe } from 'rxjs';
 import { Title } from '@angular/platform-browser';
@@ -10,10 +10,10 @@ import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-ske
 
 @Component({
   selector: 'app-pokemons-page',
-  imports: [PokemonList, PokemonListSkeleton],
+  imports: [PokemonList, PokemonListSkeleton, RouterLink],
   templateUrl: './pokemons-page.html',
 })
-export default class PokemonsPage implements OnInit {
+export default class PokemonsPage {
   // public isLoading = signal(true);// cuando se necesita el cargando en spa porque el ssr se envia la info antes por el oninit
   // private appRef = inject(ApplicationRef);
   // private $appState = this.appRef.isStable.subscribe((isStable) => {
@@ -30,39 +30,45 @@ export default class PokemonsPage implements OnInit {
   private title = inject(Title);
 
   public currentPage = toSignal<number>(
-    this.route.queryParamMap.pipe(
-      map((params) => params.get('page') ?? '1'),
+    this.route.params.pipe(
+      map((params) => params['page'] ?? '1'),
       //si no es un numero se envia 1 si es un nuumero se envia e numero
       map((page) => (isNaN(+page) ? 1 : +page)), //el + antes de page transforma en numero, remplaza number(page)
       map((page) => Math.max(1, page)), //page solo puede estar entre 1 y pages
     ),
   );
 
-  ngOnInit(): void {
-    console.log('currentpage', this.currentPage());
-    // this.route.queryParamMap.subscribe(params =>{
-    //   console.log("paramas",params)
-    // })//observable
-    this.loadPokemons();
+  public loadOnpageChanged = effect (() => {
+    console.log('paginacambio', this.currentPage())
+    this.loadPokemons(this.currentPage());
+  })
 
-    // setTimeout(() => {
-    //   this.isLoading.set(false);
-    // }, 5000);
-  }
+  // ngOnInit(): void {
+  //   console.log('currentpage', this.currentPage());
+  //   // this.route.queryParamMap.subscribe(params =>{
+  //   //   console.log("paramas",params)
+  //   // })//observable
+  //   this.loadPokemons();
+
+  //   // setTimeout(() => {
+  //   //   this.isLoading.set(false);
+  //   // }, 5000);
+  // }
 
   // ngOnDestroy(): void {
   //   this.$appState.unsubscribe();
   // }
 
   public loadPokemons(page = 0) {
-    const pageToLoad = this.currentPage()! + page;
+    // const pageToLoad = this.currentPage()! + page;
+    const pageToLoad = page;
     // this.currentPage.set no se puede hacer porque la señal esta basada en un observable
     // console.log({ pageToLoad, currentPage: this.currentPage() });
 
     this.pokemonsServices
       .loadPage(pageToLoad)
       .pipe(
-        tap(() => this.router.navigate([], { queryParams: { page: pageToLoad } })),
+        // tap(() => this.router.navigate([], { queryParams: { page: pageToLoad } })),
         tap(() => this.title.setTitle(` Pokemons SSR - Page ${pageToLoad}`)),
       ) //cambiar queryparams y no se recarga
       .subscribe((pokemons) => {
